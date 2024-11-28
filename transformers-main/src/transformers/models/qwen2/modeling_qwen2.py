fcc4b24578859ba0c83b19e965e68889ef3e1c33
Ajibola Emmanuel <89350081+AjibolaPy@users.noreply.github.com>
2024-11-28 09:02:13 -0800

    Update modeling_qwen2.py

--- transformers-main/src/transformers/models/qwen2/modeling_qwen2.py
+++ transformers-main/src/transformers/models/qwen2/modeling_qwen2.py
@@ -578,12 +578,14 @@ class QWENVLM(nn.Module):
         self.linear1=nn.Linear(ndim, ndim*2)
         self.linear2=nn.Linear(ndim, reduced)
         self.layer_norm1=nn.LayerNorm(ndim)
+        self.qwennorm= Qwen2RMSNorm(ndim, eps=config.rms_norm_eps)
+        self.qwennorm2= Qwen2RMSNorm(ndim, eps=config.rms_norm_eps)
         self.layer_norm2=nn.LayerNorm(reduced)
     def forward(self, x):
-        normalized=self.layer_norm1(x)
+        normalized=self.lqwennorm(x)
       #  print("passed here", normalized.shape)
         x1=self.linear1(normalized)
-        x2=nn.GLU(dim=-1)(x1)
+        x2=nn.GELU()(x1)
        # print("After GLU LAYER ", x2.shape)
         x3=self.linear2(x2)
        
