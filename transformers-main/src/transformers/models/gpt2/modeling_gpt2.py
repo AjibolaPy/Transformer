0eb77c9cc034fec540e99dc15c2e8976e092458d
Ajibola Emmanuel <89350081+AjibolaPy@users.noreply.github.com>
2024-11-30 22:12:34 -0800

    Update modeling_gpt2.py

--- transformers-main/src/transformers/models/gpt2/modeling_gpt2.py
+++ transformers-main/src/transformers/models/gpt2/modeling_gpt2.py
@@ -1030,7 +1030,7 @@ class GPT2Model(GPT2PreTrainedModel):
         use_cache = use_cache if use_cache is not None else self.config.use_cache
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
         vision_embeddings = self.vision_model(inputs_embeds[0])
-        inputs_embeds = torch.cat([vision_embeddings, self.embed_tokens(inputs_embeds[1]).squeeze(1)], dim=1)
+        inputs_embeds = torch.cat([vision_embeddings, self.wte(inputs_embeds[1]).squeeze(1)], dim=1)
         if input_ids is not None and inputs_embeds is not None:
             raise ValueError("You cannot specify both input_ids and inputs_embeds at the same time")
         elif input_ids is not None:
